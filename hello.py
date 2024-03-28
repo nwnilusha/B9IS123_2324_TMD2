a94c3a2e8705b79ecf94e7fc609c25b54de11d93
Nilusha Wimalasena <nw.nilusha@gmail.com>
2024-03-28 11:00:46 +0000

    Update hellow mwthod

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")#URL leading to method
 def hello(): # Name of the method
- return("Hello World!") #indent this line
+ current_date = datetime.now().strftime("%Y-%m-%d")  # Get current date in YYYY-MM-DD format
+ return("Hello Nilusha, It is a beautiful day! {current_date} ") #indent this line
 if __name__ == "__main__":
  app.run(host='0.0.0.0', port='8080') # indent this line
