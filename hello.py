f7c32ccf8d0ff4124e4bdd45a32d7d3cf3335b42
Nilusha Wimalasena <nw.nilusha@gmail.com>
2024-03-28 11:10:45 +0000

    Correct function service error

--- hello.py
+++ hello.py
@@ -1,9 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")#URL leading to method
-def hello():
-    current_date = datetime.now().strftime("%Y-%m-%d")  # Get current date in YYYY-MM-DD format
-    return f"Hello Nilusha, It is a beautiful day! {current_date}"  # Include current date in the response
-
+def hello(): # Name of the method
+ return("Hello Nilusha, It is a beautiful day! {datetime.now().strftime("%Y-%m-%d")}") #indent this line
 if __name__ == "__main__":
  app.run(host='0.0.0.0', port='8080') # indent this line
