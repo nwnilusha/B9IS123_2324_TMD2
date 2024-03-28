7e848e485d1ab8e26bf3883c99ad21d25ce9e869
Nilusha Wimalasena <nw.nilusha@gmail.com>
2024-03-28 11:04:37 +0000

    Correct hello function

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 @app.route("/")#URL leading to method
 def hello(): # Name of the method
  current_date = datetime.now().strftime("%Y-%m-%d")  # Get current date in YYYY-MM-DD format
- return("Hello Nilusha, It is a beautiful day! {current_date} ") #indent this line
+  return f"Hello Nilusha, It is a beautiful day! {current_date}"  # Include current date in the response
 if __name__ == "__main__":
  app.run(host='0.0.0.0', port='8080') # indent this line
